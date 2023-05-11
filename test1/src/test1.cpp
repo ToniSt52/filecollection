381a4cbc37fefd5e235ca91301c75d5ea93e2d31
ToniSt52 <122443192+ToniSt52@users.noreply.github.com>
2023-05-11 16:46:37 +0000

    removal

--- test1/src/test1.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <stdint.h>
-#include "test1.h"
-
-void say_hi()
-{
-    std::cout << "Project test1" << std::endl;
-}
-
--- test1/test/test1_test.cpp
+++
@@ -1,9 +0,0 @@
-#include "gtest/gtest.h"  // include the gtest functions & macros
-#include "test1.h"
-
-#include <iostream>
-
-auto main()
-{
-    std::cout << "hello world!" << std::endl
-}
