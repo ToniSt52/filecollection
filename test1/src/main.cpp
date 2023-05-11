fcf2629f0beb10953e1d6cbdc334b79775cfeb79
ToniSt52 <122443192+ToniSt52@users.noreply.github.com>
2023-05-11 13:32:11 +0000

    first test

---
+++ test1/src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <stdint.h>
+#include "test1.h"
+
+auto main() -> int
+{
+    say_hi();
+
+    return 0;
+}
+
---
+++ test1/test/test1_test.cpp
@@ -0,0 +1,3 @@
+#include "gtest/gtest.h"  // include the gtest functions & macros
+#include "test1.h"
+
